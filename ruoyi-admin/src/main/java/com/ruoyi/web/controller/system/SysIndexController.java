075e931ac9ee861f1d27c91ddc9d1b6f9fa734ab
hwy <ilovey_hwy@163.com>
2019-03-09 12:46:06 +0800

    优化系统首页

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysIndexController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysIndexController.java
@@ -2,6 +2,7 @@ package com.ruoyi.web.controller.system;
 
 import java.util.List;
 
+import com.ruoyi.framework.web.domain.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -22,23 +23,38 @@ public class SysIndexController extends BaseController {
     @Autowired
     private ISysMenuService menuService;
 
-    // 系统首页
+    /**
+     * 系统首页
+     * @param mmap 模型参数
+     * @return
+     */
     @GetMapping("/index")
     public String index(ModelMap mmap) {
         // 取身份信息
         SysUser user = getSysUser();
         // 根据用户id取出菜单
         List<SysMenu> menus = menuService.selectMenusByUser(user);
+
         mmap.put("menus", menus);
         mmap.put("user", user);
         mmap.put("copyrightYear", Global.getCopyrightYear());
+
         return "index";
     }
 
-    // 系统介绍
+    /**
+     * 系统介绍
+     * @param mmap 模型参数
+     * @return
+     */
     @GetMapping("/system/main")
-    public String main(ModelMap mmap) {
+    public String main(ModelMap mmap) throws Exception {
         mmap.put("version", Global.getVersion());
+        Server server = new Server();
+        server.copyTo();
+
+        mmap.put("server", server);
+
         return "main";
     }
 }
