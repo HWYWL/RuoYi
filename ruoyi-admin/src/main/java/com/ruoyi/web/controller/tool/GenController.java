831d697988fc2f70671792553be89542928883aa
hwy <ilovey_hwy@163.com>
2019-03-10 18:50:49 +0800

    修改参数

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/GenController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/GenController.java
@@ -58,7 +58,7 @@ public class GenController extends BaseController {
     public void genCode(HttpServletResponse response, @PathVariable("tableName") String tableName) throws IOException {
         byte[] data = genService.generatorCode(tableName);
         response.reset();
-        response.setHeader("Content-Disposition", "attachment; filename=\"ruoyi.zip\"");
+        response.setHeader("Content-Disposition", "attachment; filename=\"YI.zip\"");
         response.addHeader("Content-Length", "" + data.length);
         response.setContentType("application/octet-stream; charset=UTF-8");
 
@@ -76,7 +76,7 @@ public class GenController extends BaseController {
         String[] tableNames = Convert.toStrArray(tables);
         byte[] data = genService.generatorCode(tableNames);
         response.reset();
-        response.setHeader("Content-Disposition", "attachment; filename=\"ruoyi.zip\"");
+        response.setHeader("Content-Disposition", "attachment; filename=\"YI_batch.zip\"");
         response.addHeader("Content-Length", "" + data.length);
         response.setContentType("application/octet-stream; charset=UTF-8");
 
